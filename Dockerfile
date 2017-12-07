5003b6ff7b082e64f6ecfd82be1e1edf937b0c7a
qn4ajhxzqa <qn4ajhxzqa@yandex.ru>
2017-12-07 12:53:54 +0600

    Add docker-compose.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:latest
+
+RUN apt -y update --fix-missing
+RUN apt install -y nano git curl make python g++ nodejs npm --no-install-recommends
+
+RUN ln -s /usr/bin/nodejs /usr/bin/node
+
+RUN mkdir /var/explorer
+ADD app /var/explorer/app
+ADD .bowerrc bower.json package.json /var/explorer/
+
+EXPOSE 8080
+
+WORKDIR /var/explorer
+
+ENTRYPOINT ["npm"]
+CMD ["start"]
--- app/app.js
+++ app/app.js
@@ -3,40 +3,41 @@
 angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
 
 .config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'mainCtrl'
-            }).
-            when('/block/:blockId', {
-                templateUrl: 'views/blockInfos.html',
-                controller: 'blockInfosCtrl'
-            }).
-            when('/transaction/:transactionId', {
-                templateUrl: 'views/transactionInfos.html',
-                controller: 'transactionInfosCtrl'
-            }).
-            when('/address/:addressId', {
-                templateUrl: 'views/addressInfo.html',
-                controller: 'addressInfoCtrl'
-            }).
-            otherwise({
-                redirectTo: '/'
-            });
-    }])
-    .run(function($rootScope) {
-        var web3 = new Web3();
-        var eth_node_url = 'http://localhost:8545'; // TODO: remote URL
-	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
-        $rootScope.web3 = web3;
-        function sleepFor( sleepDuration ){
-            var now = new Date().getTime();
-            while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
-        }
-        var connected = false;
-        if(!web3.isConnected()) {
-            $('#connectwarning').modal({keyboard:false,backdrop:'static'}) 
-            $('#connectwarning').modal('show') 
-        }
-    });
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'mainCtrl'
+      }).
+      when('/block/:blockId', {
+        templateUrl: 'views/blockInfos.html',
+        controller: 'blockInfosCtrl'
+      }).
+      when('/transaction/:transactionId', {
+        templateUrl: 'views/transactionInfos.html',
+        controller: 'transactionInfosCtrl'
+      }).
+      when('/address/:addressId', {
+        templateUrl: 'views/addressInfo.html',
+        controller: 'addressInfoCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }])
+  .run(function onRun($rootScope) {
+    var web3 = new Web3();
+    var ethNodeUrl = [window.location.protocol, '//', window.location.hostname, ':8545'].join('');
+    web3.setProvider(new web3.providers.HttpProvider(ethNodeUrl));
+      $rootScope.web3 = web3;
+      function sleepFor( sleepDuration ){
+        var now = new Date().getTime();
+        while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
+      }
+      var connected = web3.isConnected();
+      if (!connected) {
+        var $connectwarning = $('#connectwarning');
+        $connectwarning.modal({keyboard:false,backdrop:'static'});
+        $connectwarning.modal('show');
+      }
+  });
